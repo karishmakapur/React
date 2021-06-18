1bfd1aa89690dd52802a589c4933f7d6be41ee19
Karishma Kapur <kkapur.1222@gmail.com>
2021-06-18 14:32:49 -0700

    map practice

--- src/index.js
+++ src/index.js
@@ -16,3 +16,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
